9b0b4af5fadb2861b53ee98c8a01c626a4302576
Kurt Seifried <kurt@seifried.org>
2022-07-04 00:08:07 -0600

    gsd id bits working

--- read-all-gsd-files-into-mega-file.py
+++ read-all-gsd-files-into-mega-file.py
@@ -7,6 +7,8 @@
 import os
 import re
 import json
+import validators
+# pip3 install validators
 
 # This only works when run from the gsd-database directory
 filesystem_path = "./"
@@ -19,7 +21,6 @@ def load_gsd_files_into_memory(path):
 			# Make sure we only load GSD files so check if starts with year
 			if re.match("\./[0-9][0-9][0-9][0-9]/.*", root):
 				gsd = file.replace(".json", "")
-				#print(gsd)
 				f = open(os.path.join(root,file))
 				gsd_data[gsd] = json.load(f)
 				f.close()
@@ -38,39 +39,69 @@ def write_data_to_json_file(json_data, filename):
 	with open(filename, 'w', encoding='utf-8') as f:
 		json.dump(all_gsd_data, f, ensure_ascii=False, indent=2)
 
-def walk_dict(data):
+# TODO: what happens if it's not a dict/list/str? what about a boolean? or INT
+def walk_dict(data, gsdkey):
 	for key,value in data.items():
 		if isinstance(value, str):
-			print ("DICT VALUE: " + str(key) + " -> " + value)
+			if str(key) == "url":
+				print (gsdkey + " " + value)
 		if isinstance(value, dict):
-			walk_dict(value)
+			walk_dict(value, gsdkey)
 		elif isinstance(value, list):
 			for val in value:
 				if isinstance(val, str):
-					print("LIST VALUE: " + val)
+					if key == "references":
+						print(gsdkey + " " + val)
 				elif isinstance(val, list):
 					pass
 				else:
-					walk_dict(val)
+					walk_dict(val, gsdkey)
+
+# To check for urls lets use the validators
+# pip3 install validators
+
+def process_gsd_data(data):
+	# Layer one: GSD entries
+	for gsdkey,gsdvalue in data.items():
+		# Layer two: GSD/OSV/namespaces
+		for rootkey,rootvalue in gsdvalue.items():
+			if rootkey == "GSD":
+				walk_dict(rootvalue, gsdkey)
+			elif rootkey == "OSV":
+				walk_dict(rootvalue, gsdkey)
+			elif rootkey == "namespaces":
+				for namespacekey,namespacevalue in rootvalue.items():
+					walk_dict(namespacevalue, gsdkey)
+			elif rootkey == "overlay":
+				# ignore for now
+				continue
+			else:
+				# print an error
+				print("ERROR, UNKNOWN DATA FOUND: " + gsdkey + " " + rootkey )
+
 
 # walk gsd mega files
 # 1) dict, first keys are GSD ids
 # 2) GSD ID dict keys: GSD, OSV, namespaces
 # 3) namespaces dict
+# 4) walk each namespace with the walk_dict
+
+# "url": str
+# "references": list
 
 
 ######################
 
 # Load all GSD files into a large dict in memory
-#all_gsd_data = load_gsd_files_into_memory(filesystem_path)
+##all_gsd_data = load_gsd_files_into_memory(filesystem_path)
 
 # Write the GSD file data dict into a single huge file
-#write_data_to_json_file(all_gsd_data, gsd_mega_file_name)
+##write_data_to_json_file(all_gsd_data, gsd_mega_file_name)
 
 # Load the mega GSD file into a large dict in memory
 all_gsd_data = load_gsd_megafile_into_memory(gsd_mega_file_name)
 
-walk_dict(all_gsd_data)
+process_gsd_data(all_gsd_data)
 
 #
 # count should always be 1 but in case it isn't let's explicitly count it
